804610dfac08c02d913b7a851d12eb116b3a31db
Sameer Anjum <109406939+samanjum@users.noreply.github.com>
2022-07-30 20:21:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+
+var randomdiceimage = "dice"+ randomNumber1 + ".png";
+var randomimagesource = "images/" + randomdiceimage;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomimagesource);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomimagesources="images/dice"+ randomNumber2 + ".png";
+var image2= document.querySelectorAll("img")[1].setAttribute("src",randomimagesources);
+
+
+
+if(randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="🚩player 1 wins"
+}
+
+else if(randomNumber1<randomNumber2) {
+    document.querySelector("h1").innerHTML="player 2 wins🚩"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
